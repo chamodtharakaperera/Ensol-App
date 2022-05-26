415b03df21aeb1015af50504d32b5d3eff95d77e
Hirunag <hiruna@orelit.com>
2022-05-26 15:01:56 +0530

    Data binding completed

--- src/components/AddData.jsx
+++ src/components/AddData.jsx
@@ -1,19 +1,75 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { Component, useState } from "react";
 import { Container, Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
-export default class AddData extends Component {
-  render() {
+const AddData = () => {
+
+   const[serialnumber,setserialNumber] = useState("");
+   const[machinename,setmachineName] = useState("");
+   const [description,setDescription] = useState("");
+   const [rentprice,setrentPrice] = useState(0);
+   const [availablequantity,setavailableQuantity] = useState(0);
+   const [image,setImage] = useState({});
+
+
+   console.log("machinePhotos"+serialnumber);
+   console.log("machinename"+machinename);
+   console.log("description"+description);
+   console.log("availablequantity"+availablequantity);
+   console.log("rentprice"+rentprice);
+   console.log("image"+image);
+  
+   
+
+   const data = {serialNumber:serialnumber,machineType:machinename,description:description,rentPrice:rentprice,availableQty:availablequantity,machinePhotos:image}
+
+   const Insert = async() => {
+
+    const token = sessionStorage.getItem("token")
+    console.log(token);
+  
+    await axios.post("https://ensolapi.herokuapp.com/machine", data ,
+      {
+        headers : {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`
+        }
+      }
+    
+     
+    ).then((response)=> {
+      console.log(response.data);
+    })
+
+  }
+
+
+   const imageCome = (img) =>{
+    setImage(img);
+   }
+   
+
+  
     return (
       <Container className="mt-3">
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>Serial Number</Form.Label>
-            <Form.Control type="text" placeholder="Enter Serial No" />
+            <Form.Control type="text" placeholder="Enter Serial No"
+           onChange={(event)=>{
+            setserialNumber(event.target.value);
+       }}
+            
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Machine Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter Machine Name" />
+            <Form.Control type="text" placeholder="Enter Machine Name"
+             onChange={(event)=>{
+              setmachineName(event.target.value);
+         }}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Description</Form.Label>
@@ -21,23 +77,40 @@ export default class AddData extends Component {
               as="textarea"
               placeholder="Enter Your Description"
               rows={3}
+              onChange={(event)=>{
+                setDescription(event.target.value);
+           }}
             />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Rent Price</Form.Label>
-            <Form.Control type="text" placeholder="Enter Rent Price" />
+            <Form.Control type="text" placeholder="Enter Rent Price" 
+             onChange={(event)=>{
+              setrentPrice(event.target.value);
+         }}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Available Quantity</Form.Label>
-            <Form.Control type="email" placeholder="Enter Available Quantity" />
+            <Form.Control type="email" placeholder="Enter Available Quantity" 
+             onChange={(event)=>{
+              setavailableQuantity(event.target.value);
+         }}
+            />
           </Form.Group>
           {/* https://react-bootstrap.github.io/forms/form-control/ */}
           <Form.Group controlId="formFileMultiple" className="mb-3">
             <Form.Label>Upload Multiple Images</Form.Label>
-            <Form.Control type="file" multiple />
+            <Form.Control type="file" multiple
+             onChange={(event)=>{
+              imageCome(event.target.files[0]);
+         }}
+            />
           </Form.Group>
           <Form.Group className="my-3">
-            <Button style={{color:"white"}} className="mr-5" variant="primary">
+            <Button 
+            onClick={Insert}
+            style={{color:"white"}} className="mr-5" variant="primary">
               Insert
             </Button>{" "}
             <Button style={{color:"white"}} className="mr-5" variant="primary">
@@ -53,5 +126,7 @@ export default class AddData extends Component {
         </Form>
       </Container>
     );
-  }
+  
 }
+
+export default AddData;
